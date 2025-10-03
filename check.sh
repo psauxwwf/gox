45220dd5838b03995b38084bc362b69c3f026e3c
psauxwwf <psauxwwf@d6f.ru>
2025-10-03 21:53:24 +0300

    fix: remove vendor
    fix
    
    fix
    
    fix

--- check.sh
+++ check.sh
@@ -7,7 +7,7 @@ arr=(
 )
 for item in "${arr[@]}"; do
 	connect="socks5h://$PROXY_USERNAME:$PROXY_PASSWORD@$item:$PROXY_PORT"
-	if curl --max-time 1 -x "$connect" ident.me >/dev/null 2>&1; then
+	if curl --max-time 3 -x "$connect" ident.me >/dev/null 2>&1; then
 		echo "[OK] $connect"
 	else
 		echo "[FAILED] $item"
