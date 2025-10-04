0ff23d815135d2a702a4785f88917344b3aa299c
psauxwwf <psauxwwf@d6f.ru>
2025-10-04 20:37:17 +0300

    fix
    fix

--- check.sh
+++ check.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 PROXY_USERNAME=********
 PROXY_PASSWORD=********
-PROXY_PORT=31080
+SOCKS_PROXY_PORT=1080
+HTTPS_PROXY_PORT=8443
 arr=(
 	"**************"
 )
+
 for item in "${arr[@]}"; do
-	connect="socks5h://$PROXY_USERNAME:$PROXY_PASSWORD@$item:$PROXY_PORT"
+
+	connect="socks5h://$PROXY_USERNAME:$PROXY_PASSWORD@$item:$SOCKS_PROXY_PORT"
 	if curl --max-time 3 -x "$connect" ident.me >/dev/null 2>&1; then
 		echo "[OK] $connect"
 	else
-		echo "[FAILED] $item"
+		echo "[FAILED] $connect"
 	fi
+
+	connect="https://$PROXY_USERNAME:$PROXY_PASSWORD@$item:$HTTPS_PROXY_PORT"
+	if curl --max-time 3 -x "$connect" --proxy-insecure -k ident.me >/dev/null 2>&1; then
+		echo "[OK] $connect"
+	else
+		echo "[FAILED] $connect"
+	fi
+
 done
